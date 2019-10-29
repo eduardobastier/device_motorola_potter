8e6555e52f15ffc6775bd513a043dd8096a36a5a
ZJRDroid <priv.zjr@gmail.com>
2019-10-29 19:58:36 +0100

    Build android.hardware.camera.provider@2.4-service

--- device.mk
+++ device.mk
@@ -112,6 +112,8 @@ PRODUCT_PACKAGES += \
     libbson \
     libjustshoot_shim \
     Snap \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
     libgui_vendor
